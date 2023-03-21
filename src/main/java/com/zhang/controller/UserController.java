8d5d0fced2b7d8c62ce0ac0161fb2fbb87c791a6
ZhangJR <2283833968@qq.com>
2023-03-21 17:02:32 +0800

    third commit

--- src/main/java/com/zhang/controller/UserController.java
+++ src/main/java/com/zhang/controller/UserController.java
@@ -36,7 +36,7 @@ public class UserController {
 
     @RequestMapping("/jsonuser")
     public String jsonuser(@RequestBody User user){
-        return "{'jsonuser':'springmv'}";
+        return "{'jsonuser':'springmvdsadsa'}";
     }
 
 }
