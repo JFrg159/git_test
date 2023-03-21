fed51bf51ec189e8b40a0d083e7ba03cbbfa5c7b
ZhangJR <2283833968@qq.com>
2023-03-21 16:58:03 +0800

    secnod commit

--- src/main/java/com/zhang/controller/UserController.java
+++ src/main/java/com/zhang/controller/UserController.java
@@ -36,7 +36,7 @@ public class UserController {
 
     @RequestMapping("/jsonuser")
     public String jsonuser(@RequestBody User user){
-        return "{'jsonuser':'springmvc'}";
+        return "{'jsonuser':'springmv'}";
     }
 
 }
